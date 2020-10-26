afa7ed0098e144546ef8078290e71ef84164ef24
Julia Nikolaeva <iamenates@gmail.com>
2020-10-27 00:32:39 +0300

    added files lesson2 and homework for lesson2

---
+++ src/Homework/Lesson2.java
@@ -0,0 +1,73 @@
+package Homework;
+
+public class Lesson2 {
+     public static void main(String[] args) {
+         byte b1 = 12;
+         byte b2 = 0B1100;
+         byte b3 = 014;
+         byte b4 = 0XC;
+         
+         short s1 = 1300;
+         short s2 = 0B101_0001_0100;
+         short s3 = 02424;
+         short s4 = 0X514;
+         
+         int i1 = 0;
+         int i2 = 0B0;
+         int i3 = 00;
+         int i4 = 0X0;
+         
+         long l1 = 123456789;
+         long l2 = 0B111_0101_1011_1100_1101_0001_0101;
+         long l3 = 0_726_746_425;
+         long l4 = 0X75B_CD15;
+         
+         float f1 = 0.5F;
+         float f2 = 1.2F;
+         
+         double d1 = 45.89;
+         double d2 = 59.10;
+         
+         boolean bool1 = true;
+         boolean bool2 = false;
+         
+         char c1 = '3';
+         char c2 = 'f';
+         char c3 = 56723;
+         char c4 = '\u4993';
+         
+         System.out.println(b1);
+         System.out.println(b2);
+         System.out.println(b3);
+         System.out.println(b4);
+         
+         System.out.println(s1);
+         System.out.println(s2);
+         System.out.println(s3);
+         System.out.println(s4);
+         
+         System.out.println(i1);
+         System.out.println(i2);
+         System.out.println(i3);
+         System.out.println(i4);
+         
+         System.out.println(l1);
+         System.out.println(l2);
+         System.out.println(l3);
+         System.out.println(l4);
+         
+         System.out.println(f1);
+         System.out.println(f2);
+         
+         System.out.println(d1);
+         System.out.println(d2);
+         
+         System.out.println(bool1);
+         System.out.println(bool2);
+         
+         System.out.println(c1);
+         System.out.println(c2);
+         System.out.println(c3);
+         System.out.println(c4);
+     }    
+}
